fa5a0e178d2a9630c47ea83573350e017bd98e1b
Fernando Marins <f.augustomarins@gmail.com>
2024-09-21 19:08:04 -0300

    Organizing the code

---
+++ sangoma.py
@@ -0,0 +1,71 @@
+
+sangoma = {
+    "bones": [
+        {
+            "name": "Aliança",
+            "description": "Relações amorosas"
+        },
+        {
+            "name": "Botão",
+            "description": "Todo tipo de relação, seja de negócios, amizades, se estiver junto da aliança é relação muito forte",
+        },
+        {
+            "name": "Crucifixo",
+            "description": "Espiritualidade, caminhos, impedimentos na vida, sofrimentos, tudo depende de qual parte que cai ou qual osso tá junto",
+        },
+        {
+            "name": "Concha",
+            "description": "Estado emocional, relações amorosas, mas na pessoa em si, não o casal",
+        },
+        {
+            "name": "Chave",
+            "description": "Projetos, negócios, casas, carro, tudo que tange o comércio, caminhos, possiblidade, oportunidades",
+        },
+        {
+            "name": "Moeda",
+            "description": "Prosperidade, negócios, comércio, dívidas que precisam ser pagas (se cai moeda, crucifixo e caveira, é dívida pra espírito)",
+        },
+        {
+            "name": "Dado",
+            "description": "Mundo dos prazeres, noitadas, bares, da sorte. Tem também o simbolismo numerológico. Pode falar de tempo",
+        },
+        {
+            "name": "Fícula da sorte",
+            "description": "Família, estrutura, boa sorte, riquezas, heranças, tudo o que cai sobre os ombros de uma pessoa",
+        },
+        {
+            "name": "4 búzios",
+            "description": "Para fazer confirmações"
+        },
+        {
+            "name": "Prego",
+            "description": "Ataques mágicos e etc"
+        },
+        {
+            "name": "Dente de cavalo",
+            "description": "Soberania, proteção, liberdade financeira",
+        },
+    ],
+    "buzios": [
+        {
+            "name": "4 abertos",
+            "description": "Aláfia"
+        },
+        {
+            "name": "3 abertos e 1 fechado",
+            "description": "Não"
+            },
+        {
+            "name": "2 abertos e 2 fechados",
+            "description": "Sim"
+        },
+        {
+            "name": "1 aberto e 3 fechados",
+            "description": "Sim, mas falta algo"
+        },
+        {
+            "name": "4 fechados",
+            "description": "Perigo, negativo ou algo que Exú quer dizer como alerta",
+        },
+    ],
+}
