0da2276e594fd41cc979c861eb288ad99769dfaa
Fernando Marins <f.augustomarins@gmail.com>
2024-10-03 20:05:21 -0300

    Adding herbs.py back

---
+++ herbs.py
@@ -0,0 +1,52 @@
+herbs = {
+    "herbs": [
+        {
+            "name": "Aroeira",
+            "scientific_name": "Schinus lotofacil therebenthifolius",
+            "type": "Quente",
+            "description": "A folha de Aroeira é força pura no preparo de banhos de limpeza (com parcimônia, em razão da toxicidade) e nas defumações. Os banhos podem ser preparados de duas formas distintas: através da maceração das folhas ou da decocção das cascas. Em ambos os casos, a água será imantada com uma energia capaz de afastar obsessores, pragas e maldições. O sumo dos frutos esmagados pode ser usado em comunhão com a tinta de urucum pra traçar pontos mágicos poderososos.\n\nO rapé feito de cinzas de aroeira pode ser comparado a uma ventania que levanta a poeira para mostrar o que está oculto. Apesar de suas propriedades adstringente, balsâmica, diurética, anti-inflamatória, antimicrobiana, tônica e cicatrizante, seu efeito espiritual está em mostrar aquilo que não sabemos e curar através da energia da mata. Essa medicina pode promover a limpeza através do vômito e diarréia."
+        },
+        {
+            "name": "Arruda",
+            "scientific_name": "Ruta graveolens",
+            "type": "Quente",
+            "description": "É usada em banhos, defumações e rituais de 'Bate Folhas' para proporcionar limpeza de todas as energias que atacam o escudo energético. Seu perfume afasta, repete espíritos obsessores e vingativos. No mundo esotérico, possui fortes conexões com as energias do mundo dos mortos. A raiz dessa planta pode ser usada tanto para quebras maldições quanto para enviá-las aos inimigos. Não podemos usá-las em gestantes, pois é uma espécie com energia abortiva."
+        },
+        {
+            "name": "Beladona",
+            "scientific_name": "Atropa belladonna",
+            "type": "Quente",
+            "description": "É uma espécie estrangeira que, por ser muito utilizada nas práticas de bruxaria medievel, acabou sendo inserida nas sendas espirituais brasileiras. A energia feminina e lunar de seu Elemental tem sido fruto de estudo, admiração e temor, afinal, uma de suas características é o entorpecimento sensorial. É tóxica e narcótica, portanto, seu uso deve ser extremamente cauteloso. Entretanto, se colocarmos pequenos pedaços de folhas em banhos revitalizantes, avivaremos a felicidade e a autoestima. Isso é importantíssimo nos processos depressivos. A defumação com folhas e flores secas tem poder de despertar a visão espiritual. Se fizermos um chá com frutos macerados e folhas e, com ajuda de um pano, realizarmos a limpeza de uma residência, os fios existentes nas energias nocivas são cortados. Palavra-chave: Parcimônia. Observação: Não a confundam com a 'Maria-pretinha'."
+        },
+        {
+            "name": "Buchinha-do-Norte",
+            "scientific_name": "Luffa operculata",
+            "type": "Quente",
+            "description": "Essa planta, de aspecto 'inofensivo', possui fortes toxinas capazes de causar aborto (assim como a arruda). Dentro dos processos externos de limpeza (banhos e defumações) tem uma ação contundente contra vampirismos e ataques espirituais. Podemos usá-la em banhos conjuntamente com sabões religiosos (exemplo: sabão-da-costa). Queimá-la é o mesmo que aplicar contra baixas vibrações. Se colocarmos uma buchinha encharcada de óleo-de-dendê (epô) dentro de um alguidar e deixarmos na porta de entrada de uma residência, produziremos um feitiço capaz de absorver energias desequilibradas (inveja, ódio e pragas). Depois de 7 dias a queimamos em local aberto.\n\nComo toda bucha natural, associa-se ao casulo das aranhas, podendo ser usada em feitiços de dominação. Nos banhos e defumações usamos a planta inteira (inclusive as cascas secas."
+        },
+        {
+            "name": "Cactos",
+            "scientific_name": "Cactaceae",
+            "type": "Quente",
+            "description": "Essas espécies são plantas que vivem em situações extremas e sofreram adaptações ao longo dos séculos. Existem muitos usos mágicos para tais plantas, entretanto, usamos o caule picado em banhos de proteção espiritual, principalmente quando estamos convivendo com situações graves (perseguições, injustiças, ódio). Com os espinhos secos e triturados podemos fazer um pó com qualidades de proteção (soprado nas portas das residências), mas se misturarmos com o pó de canela torna-se um excelente atrativo de boa sorte.\n\nO rapé de Cactos Mandacaru é uma medicina que concede resistência ao organismo, além de purificá-lo intensamente. É tonificante, além de cardiotônico. Trabalha intensamente o vórtice cardíaco e, consequentemente, ajuda nos processos de autocontrole e autoconhecimento. Seu uso em rituais alivia-nos proporcionando regeneração."
+        },
+        {
+            "name": "Casca de alho",
+            "scientific_name": "Allium",
+            "type": "Quente",
+            "description": "Provavelmente esse seja um dos elementos mais fáceis de se recorrer nos casos de extrema necessidade. Repleta de energia ígnea, com boa porcentagem de enxofre em sua composição, a casca de alho tem poder de dissolver, libertar e desintoxicar, ou seja, ministrada corretamente, limpa o corpo ou ambiente, libertando-o de todas as correntes nocivas (advindas de obsessores vampíricos) e desintoxica (inicia o processo de proteção natural). Recomendada para banhos, defumações, bem como na preparação de tinturas de limpeza."
+        },
+        {
+            "name": "Casca de cebola roxa",
+            "scientific_name": "Allium cepa Lineu",
+            "type": "Quente",
+            "description": "Assim como as cascas de alho, as cascas de cebola roxa são podersas aliadas nos processos de limpeza. Os principais benefícios dessa planta são seus efeitos anti-inflamatórios e circulatórios, ou seja, ao queimarmos ou as usarmos em banhos (tanto no corpo como nos ambientes) sentiremos a diminuição das dores corporais, bem como restabeleceremos todo processo energético do organismo. A casca de cebola roxa é um agente primordial nos processos de limpeza, pois abre espaço para que as demais plantas atuem. Não usar em muita quantidade nos banhos, pois, pode manchar a pele."
+        },
+        {
+            "name": "Comigo-Ninguém-Pode",
+            "scientific_name": "Dieffennachia pictada",
+            "type": "Quente",
+            "description": "Essa planta é uma das espécies mais usadas na feitiçaria Botânica. Suas folhas possuem alto grau de toxidade, entretanto, o banho e a defumação feitos com essa planta são recomendados contra a ação de energias nocivas originadas por ataques voluntários e involuntários.\n\nPeles sensíveis podem apresentar fortes irritações, coceiras e até leves queimaduras quando entram em contato com as folhas de comigo-ninguém-pode, entretanto, cabe-nos sempre moderar ou até testá-las na pele antes de ministrarmos. Usada em Vasos de Poder nos comércios como protetora e, ao mesmo tempo, como energia seletiva para clientes que realmente desejam comprar, o uso dessa planta é muito amplo.\n\nAlgo relevante é saber diferenciar as 'fêmeas' (usadas para energia feminina) das folhas 'machos' (usadas para energia masculina). As folhas fêmeas possuem pintinhas ou manchas mais claras e as machos são completamente verdes. Porém, independentemente dessa diferença, o uso da planta não é condicionado à polaridade, assim, o sexo da folha não atrapalhará o resultado desejado, apenas potencializará ainda mais. Essa planta também possui atributos mágicos para os feitiços sentimentais. Ao unirmos as duas polaridades (macho e fêmea) regando-as abundantemente com mel, colocando o nome ou foto do casal entre as folhas realizaremos um feitiço que protege a relação e pode até reverter processos de separação. Esse feitiço deve ser colocado aos pés de uma árvore frutífera."
+        },
+    ]
+}
