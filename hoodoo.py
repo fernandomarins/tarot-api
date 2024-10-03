9050c80f9a0d8aaf0387c527abe86fc0649a04f2
Fernando Marins <f.augustomarins@gmail.com>
2024-10-03 20:32:56 -0300

    Changing

--- hoodoo.py
+++ hoodoo.py
@@ -6,16 +6,15 @@ hoodoo = {
               "type": "limpeza",
               "materials": [
                             {
-                                "name":"vela",
+                                "name":"Vela",
                                 "quantity":"1"
                             },
                             {
-                                "name": "alfinete",
-                                "quantity": "de acordo com o nome do alvo"
+                                "name": "Alfinete",
+                                "quantity": "De acordo com o nome do alvo"
                             },
                             {
-                                "name": "óleo de banimento",
-                                "quantity": "1"
+                                "name": "Óleo de banimento",
                             }
                            ],
               "procedure": "Este é um feitiço muito versátil que pode ser usado para banir pessoas, coisas que você precisar. Você vai escrever o nome (inimigo) em uma vela preta, vista com óleo de banimento. Alguns conjures escolhem escrever o nome de cabeça para baixo e de trás para a frente. A escolha, no entanto, é sua. Pegue os alfinetes e introduza-os através da vela entre cada letra do nome. Se você puder obter qualquer link pessoal, envolva-os em uma petição por escrito colocada sob a vela. À medida que a vela diminuir a cada letra do nome, faça sua oração para que ele saia. Você pode dizer algo como:\n\n'Senhor, como você fez o cego ver, deixe (fulano) ver que ele não pertence mais aqui; como você fez o coxo andar, deixe-o sair de minha vida para sempre; ao curar os enfermos, cure as feridas que (fulano) infligiu sobre mim e minha família, etc.…'\n\nComo você pode ver, você pode adaptar o texto de sua oração às suas necessidades específicas.\n\nQuando a vela queimar, prenda os alfinetes na petição, despache tudo em um rio, cachoeira e vá embora sem olhar para trás."
