b8768d07919865162eb6f28d7f26dcd5b7e1b655
Fernando Marins <f.augustomarins@gmail.com>
2024-10-03 21:06:31 -0300

    Adding oils

--- hoodoo.py
+++ hoodoo.py
@@ -6,12 +6,10 @@ hoodoo = {
              "type": "limpeza",
              "materials": [
                            {
-                           "name":"Vela",
-                           "quantity":"1"
+                           "name":"1 vela preta"
                            },
                            {
-                           "name": "Alfinete",
-                           "quantity": "de acordo com o nome do alvo"
+                           "name": "Alfinetes, de acordo com o nome do alvo"
                            },
                            {
                            "name": "Óleo de banimento",
@@ -20,27 +18,49 @@ hoodoo = {
              "procedure": "Este é um feitiço muito versátil que pode ser usado para banir pessoas, coisas que você precisar. Você vai escrever o nome (inimigo) em uma vela preta, vista com óleo de banimento. Alguns conjures escolhem escrever o nome de cabeça para baixo e de trás para a frente. A escolha, no entanto, é sua. Pegue os alfinetes e introduza-os através da vela entre cada letra do nome. Se você puder obter qualquer link pessoal, envolva-os em uma petição por escrito colocada sob a vela. À medida que a vela diminuir a cada letra do nome, faça sua oração para que ele saia. Você pode dizer algo como:\n\n\"Senhor, como você fez o cego ver, deixe (fulano) ver que ele não pertence mais aqui; como você fez o coxo andar, deixe-o sair de minha vida para sempre; ao curar os enfermos, cure as feridas que (fulano) infligiu sobre mim e minha família, etc.…\"\n\nComo você pode ver, você pode adaptar o texto de sua oração às suas necessidades específicas.\n\nQuando a vela queimar, prenda os alfinetes na petição, despache tudo em um rio, cachoeira e vá embora sem olhar para trás."
              },
              {
-             "name": "Óleo de Banimento",
+             "name": "Óleo de banimento",
              "type": "óleo",
              "materials": [
                            {
-                           "name":"Incenso de olíbano",
-                           "quantity":"1/2 parte"
+                           "name":"1/2 parte de incenso de Olíbano"
                            },
                            {
-                           "name": "Alecrim",
-                           "quantity": "1/8 parte"
+                           "name": "1/8 parte de Alecrim"
                            },
                            {
-                           "name": "Louro",
-                           "quantity": "Angélica"
+                           "name": "1/8 de Louro"
                            },
                            {
-                           "name": "Manjericão",
-                           "quantity": "1/8 parte"
+                           "name": "1/8 parte de Angélica"
+                           },
+                           {
+                           "name": "1/8 parte de Manjericão"
                            }
                            ],
              "use": "Para banir qualquer coisa indesejada em sua vida."
+             }
+                          ,
+             {
+             "name": "Óleo de proteção",
+             "type": "óleo",
+             "materials": [
+                           {
+                           "name":"Alho"
+                           },
+                           {
+                           "name": "Arruda"
+                           },
+                           {
+                           "name": "Tabaco"
+                           },
+                           {
+                           "name": "Sal grosso"
+                           },
+                           {
+                           "name": "Azeite de oliva"
+                           }
+                           ],
+             "use": "Para se proteger de energias e pessoas negativas."
              }
              ]
 }
